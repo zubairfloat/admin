70b897fd0ec860916010fc6ee4e261a1d710f2ee
Zubair Rizwan <zubairfloat@gmail.com>
2020-09-15 12:39:16 +0500

    deploy

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ import * as _redux from "./redux";
 import store, { persistor } from "./redux/store";
 import App from "./app/App";
 import "./index.scss"; // Standard version
-// import "./sass/style.react.rtl.css"; // RTL version
-import "./_metronic/_assets/plugins/keenthemes-icons/font/ki.css";
-import "socicon/css/socicon.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
 
 // Datepicker
 import "react-datepicker/dist/react-datepicker.css";
@@ -25,28 +21,8 @@ import {
 } from "./_metronic/layout";
 import { MetronicI18nProvider } from "./_metronic/i18n";
 
-/**
- * Base URL of the website.
- *
- * @see https://facebook.github.io/create-react-app/docs/using-the-public-folder
- */
 const { PUBLIC_URL } = process.env;
-
-/**
- * Creates `axios-mock-adapter` instance for provided `axios` instance, add
- * basic Metronic mocks and returns it.
- *
- * @see https://github.com/ctimmerm/axios-mock-adapter
- */
-/* const mock = */ _redux.mockAxios(axios);
-
-/**
- * Inject metronic interceptors for axios.
- *
- * @see https://github.com/axios/axios#interceptors
- */
 _redux.setupAxios(axios, store);
-
 ReactDOM.render(
   <MetronicI18nProvider>
     <MetronicLayoutProvider>
